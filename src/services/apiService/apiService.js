7bce45a81ab77207b83b32119548432041812dba
Abinaya Sadhasivam <abinaya.sadhasivam@solitontech.local>
2020-07-09 20:17:59 +0530

    Add service to handle the API request and response
    Signed-off-by: Abinaya Sadhasivam <abinaya.sadhasivam@solitontech.local>

---
+++ src/services/apiService/apiService.js
@@ -0,0 +1,47 @@
+export const apiFetch = async (
+  endpoint,
+  requestType,
+  data,
+) => {
+  let apiEndpoint = endpoint;
+  try {
+    let request = await getRequest(requestType, data);
+    let result = await fetch(apiEndpoint, request);
+    result = handleValidResponse(result);
+    return result.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+async function getRequest(requestType, body) {
+  let request = {
+    method: requestType,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+  };
+
+  if (body) {
+    request.body = JSON.stringify(body);
+  }
+
+
+  return Promise.resolve(request);
+}
+
+function handleValidResponse(result) {
+  if (result.status >= 200 && result.status < 300) {
+    return result;
+  }
+
+  if (!result.status) {
+    let error = new Error("Backend not responding");
+    error.response = result;
+    throw error;
+  }
+
+  let error = new Error(result.statusText);
+  error.response = result;
+  throw error;
+}
